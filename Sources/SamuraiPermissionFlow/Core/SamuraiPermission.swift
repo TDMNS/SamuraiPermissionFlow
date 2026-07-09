d5aacdf2f671338e626ace5a4906653aa7f20c47
TDMNS <773o@mail.ru>
2026-07-09 23:35:02 +0700

    Add microphone permission provider

--- Sources/SamuraiPermissionFlow/Core/SamuraiPermission.swift
+++ Sources/SamuraiPermissionFlow/Core/SamuraiPermission.swift
@@ -9,4 +9,8 @@ import Foundation
 
 public enum SamuraiPermission {
     public static let camera = CameraPermission()
+    
+    #if os(iOS)
+    public static let microphone = MicrophonePermission()
+    #endif
 }
---
+++ Sources/SamuraiPermissionFlow/Permissions/MicrophonePermission.swift
@@ -0,0 +1,58 @@
+//
+//  MicrophonePermission.swift
+//  SamuraiPermissionFlow
+//
+//  Created by Ksyuleg on 09.07.2026.
+//
+
+#if os(iOS)
+
+import AVFoundation
+
+public struct MicrophonePermission: PermissionProvider {
+
+    public init() {}
+
+    public var status: PermissionStatus {
+        get async {
+            Self.mapStatus(
+                AVAudioSession.sharedInstance().recordPermission
+            )
+        }
+    }
+
+    public func request() async -> PermissionStatus {
+        let currentStatus = AVAudioSession.sharedInstance().recordPermission
+
+        switch currentStatus {
+        case .granted:
+            return .authorized
+        case .denied:
+            return .denied
+        case .undetermined:
+            let granted = await withCheckedContinuation { continuation in
+                AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                    continuation.resume(returning: granted)
+                }
+            }
+            return granted ? .authorized : .denied
+        @unknown default:
+            return .denied
+        }
+    }
+
+    private static func mapStatus(_ status: AVAudioSession.RecordPermission) -> PermissionStatus {
+        switch status {
+        case .undetermined:
+            return .notDetermined
+        case .denied:
+            return .denied
+        case .granted:
+            return .authorized
+        @unknown default:
+            return .denied
+        }
+    }
+}
+
+#endif
